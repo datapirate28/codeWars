da8d3a9cf427e4748e5380fb7bf6ab521c5c6a3f
datapirate28 <hashmatullah.naybi28@gmail.com>
2024-08-27 04:04:16 +0300

    started code wars and got done with the first problem

---
+++ main.js
@@ -0,0 +1,11 @@
+// code wars dialy problems
+
+// first problem: get to work the code below
+// answer
+
+// in order for this code to work, we need to return the value of multiplication 
+
+function multiply(a, b){
+    return a*b
+}
+
