76155da42d07b94d385483d6090d266fb7c547a7
datapirate28 <hashmatullah.naybi28@gmail.com>
2024-09-10 18:51:13 +0300

    done with a bonus problem for the day

--- main.js
+++ main.js
@@ -305,3 +305,23 @@ function findSummation(userInput){
 // another way of dealing with this problem:
 
 const summation = num => num * (num + 1)/2;
+
+
+// ==========
+// ==========
+
+// twelveth problem
+//Messi is a soccer player with goals in three leagues:
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+// 5, 10, 2  -->  17
+
+function goals(laligGoals, copaDelReyGoals, championsLeagueGoals){
+    return Number(laligGoals+copaDelReyGoals+championsLeagueGoals);
+}
